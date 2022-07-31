43a2eb6ba2e15cf7905687ef1b762ab5bcf9b404
yatsenkodenys <108423461+yatsenkodenys@users.noreply.github.com>
2022-07-31 21:48:26 +0300

    create super power generator

---
+++ GameForHomeWork/SuperPoverGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameForHomeWork
+{
+    public static class SuperPoverGenerator
+    {
+        public static int spg()
+        {
+            return new Random().Next(5,10);
+        }
+    }
+}
